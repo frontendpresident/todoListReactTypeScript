257db45b5564afad7458a1136df8e9345635b4d9
Sergey Maifetov <smaifetov.exceedteam@gmail.com>
2021-07-14 10:19:09 +0300

    created addTsk and taskItem components

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CreateTask from './components/CreateTask';
+
+function App () {
+  return (
+    <div className="wrapper">
+     <CreateTask />
+    </div>
+  );
+}
+
+export default App;
